f8824621e94ed558f8be3640b57370ea96144c00
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-01-06 21:23:54 +0900

    add struct

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/ChimeraCoder/anaconda"
+	"io/ioutil"
+	"log"
 )
 
+type twitterAuth struct {
+	ConsumerKey       string `json: "consumerKey"`
+	ConsumerSecret    string `json: "consumerSecret"`
+	AccessToken       string `json: "accessToken"`
+	AccessTokenSecret string `json: "accessTokenSecret"`
+}
+
 func main() {
-	anaconda.NewTwitterApiWithCredentials("access-token", "access-token-secret", "consumer-key", "consumer-secret")
+	auth, err := ioutil.ReadFile("sample.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	anaconda.SetConsumerKey("consumerkey")
+	anaconda.SetConsumerSecret("consumersecret")
+	api := anaconda.NewTwitterApi("accesstoken", "accesstokensecret")
+
 }
